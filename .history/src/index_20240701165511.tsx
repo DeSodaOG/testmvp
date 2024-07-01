bc9a92a9e9f6f173cb00bad0a0e74b4192c873b0
depointhomepage <web3mario@proton.me>
2024-07-01 17:30:15 +0800

    update

---
+++ .history/src/index_20240701165511.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: depointhomepage web3mario@proton.me
+ * @Date: 2024-07-01 16:48:13
+ * @LastEditors: depointhomepage web3mario@proton.me
+ * @LastEditTime: 2024-07-01 16:55:11
+ * @FilePath: /reactjs-template/src/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import ReactDOM from 'react-dom/client';
+
+import { Root } from '@/components/Root';
+
+// Uncomment this import in case, you would like to develop the application even outside
+// the Telegram application, just in your browser.
+// import './mockEnv.ts';
+
+import '@telegram-apps/telegram-ui/dist/styles.css';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<Root/>);
---
+++ .history/vite.config_20240701165030.ts
@@ -0,0 +1,35 @@
+/*
+ * @Author: depointhomepage web3mario@proton.me
+ * @Date: 2024-07-01 16:48:13
+ * @LastEditors: depointhomepage web3mario@proton.me
+ * @LastEditTime: 2024-07-01 16:50:29
+ * @FilePath: /reactjs-template/vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/testtma/',
+  plugins: [
+    // Allows using React dev server along with building a React application with Vite.
+    // https://npmjs.com/package/@vitejs/plugin-react-swc
+    react(),
+    // Allows using the compilerOptions.paths property in tsconfig.json.
+    // https://www.npmjs.com/package/vite-tsconfig-paths
+    tsconfigPaths(),
+    // Allows using self-signed certificates to run the dev server using HTTPS.
+    // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
+    // basicSsl(),
+  ],
+  publicDir: './public',
+  server: {
+    // Uncomment this line if you want to expose your dev server and access it from the devices
+    // in the same network.
+    // host: true,
+  },
+});
+
